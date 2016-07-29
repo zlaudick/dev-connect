8f232e079f64fff17e71becf3eef36bb7c3f72f1
Zac Laudick <zlaudick@nmsu.edu>
2016-07-29 12:49:28 -0600

    keep ideas on one list

--- epic/dev-connect-summary.php
+++ epic/dev-connect-summary.php
@@ -11,40 +11,7 @@
 		<p>Dev Connect is a website that connects web developers with small or non-profit organizations that need an online presence. Dev Connect will allow web developers to search projects posted by organizations in need of this content. The businesses can also search for web developers and view their portfolios to find preferred design styles.</p>
 		<p>This site will be designed to promote a symbiotic relationship between organizations that have limited funding for a professional website who need volunteer or low cost work, and entry to mid level web developers looking for work experience and a way to improve and showcase their skills.</p>
 		<h2>Ideas</h2>
-		<h3>Gerald Sandoval</h3>
 		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-		</ul>
-		<h3>Zac Laudick</h3>
-		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-		</ul>
-		<h3>Marcelo Ibarra</h3>
-		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-		</ul>
-		<h3>Devon Beets</h3>
-		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
 			<li></li>
 		</ul>
 	</body>
