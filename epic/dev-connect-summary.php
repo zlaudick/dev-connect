7edaf93e0009e4d618951face6777eddc525550d
Gerald Sandoval <gsandoval16@cnm.edu>
2016-07-31 10:57:10 -0600

    Gerald: my 2 cents

--- epic/dev-connect-summary.php
+++ epic/dev-connect-summary.php
@@ -11,6 +11,9 @@
 		<p>Dev Connect is a website that connects web developers with small or non-profit organizations that need an online presence. Dev Connect will allow web developers to search projects posted by organizations in need of this content. The businesses can also search for web developers and view their portfolios to find preferred design styles.</p>
 		<p>This site will be designed to promote a symbiotic relationship between organizations that have limited funding for a professional website who need volunteer or low cost work, and entry to mid level web developers looking for work experience and a way to improve and showcase their skills.</p>
 		<h2>Ideas</h2>
+		<h3>Gerald: I rewrote the summary. If you like it, great. if not, that's cool too.</h3>
+		<p>The goal of this project is to connect web developers with small or non-profit organizations in need an online presence.</p>
+		<p>The intent of this site will be to create a sybiotic relationship between web developers and organizations.  Web developers, who wish to build their portfolios and to increase their skills and marketability, will have to opportunity to search and work pro bono for a cause they wish to support.  Organizations, such as a non-profit, will have the opportunity to showcase their cause and their online needs.  Small organizations will also have the opportunity to post their online needs and propose an alternative compensation plan.</p>
 		<ul>
 			<li></li>
 		</ul>
