ebf85eb3c086107b9e4fa75ef85d830f00363d60
Devon Beets <dbeetzz@gmail.com>
2016-07-31 16:09:43 -0600

    added some of the main points of the design for the website

--- epic/dev-connect-summary.php
+++ epic/dev-connect-summary.php
@@ -13,9 +13,11 @@
 		<h2>Ideas</h2>
 		<h3>Gerald: I rewrote the summary. If you like it, great. if not, that's cool too!</h3>
 		<p>The goal of this project is to connect web developers with small or non-profit organizations in need an online presence.</p>
-		<p>The intent of this site will be to create a sybiotic relationship between web developers and organizations.  Web developers, who wish to build their portfolios and to increase their skills and marketability, will have to opportunity to search and work pro bono for a cause they wish to support.  Organizations, such as a non-profit, will have the opportunity to showcase their cause and their online needs.  Small organizations will also have the opportunity to post their online needs and propose an alternative compensation plan.</p>
+		<p>The intent of this site will be to create a symbiotic relationship between web developers and organizations.  Web developers, who wish to build their portfolios and to increase their skills and marketability, will have to opportunity to search and work pro bono for a cause they wish to support.  Organizations, such as a non-profit, will have the opportunity to showcase their cause and their online needs.  Small organizations will also have the opportunity to post their online needs and propose an alternative compensation plan.</p>
 		<ul>
-			<li></li>
+			<li>We will have two primary users, developers and organizations. They will be able to use the site to interact with each other and share content and needs</li>
+			<li>There will be a jobs board, where organizations can post project and link them to their profile where interested developers can contact them.</li>
+			<li>Devs will be able to link their work to their profile, so organizations can check it out easily.</li>
 		</ul>
 	</body>
 </html>
