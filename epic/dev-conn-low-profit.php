d0061eff97a8f8ffbddf795e5fb2f387f9f3a930
Devon Beets <dbeetzz@gmail.com>
2016-08-02 15:49:06 -0600

    added low profit attitudes and behaviors, frustrations and needs, and goals and synced formatting for use case

--- epic/dev-conn-low-profit.php
+++ epic/dev-conn-low-profit.php
@@ -12,9 +12,14 @@
 		<p><strong>Name:</strong> Frank Marlin</p>
 		<p><strong>Age:</strong> 52</p>
 		<p><strong>Profession:</strong> Owner of the Three Amigo's Restaurant, a small sandwich and soup kitchen. A busy man without a lot of time on his hands, he does a lot of the work himself with a few family members and some part time employees. He's been losing business due to rising competition from franchises popping up near his location, and decided that he needs an online presence and ordering system to stay competitive. He does not have a lot of money for this, but can pay someone a small sum to build his site. He has thought about using Square Space, but does not have the time to even build a site with that as he works seven days a week.</p>
-		<p><strong>Technology:</strong>A simple HP desktop computer at his workplace, used to place orders for new food and restaurant supplies. And a Dell notebook laptop for personal use at home. Also has a Samsung Galaxy S4 smart phone he uses primarily for texting and checking emails.</p>
-		<p><strong>Attitudes and Behaviors:</strong></p>
-		<p><strong>Frustrations and Needs:</strong></p>
-		<p><strong>Goals:</strong></p>
+		<p><strong>Technology:</strong> A simple HP desktop computer at his workplace, used to place orders for new food and restaurant supplies. And a Dell notebook laptop for personal use at home. Also has a Samsung Galaxy S4 smart phone he uses primarily for texting and checking emails.</p>
+		<p><strong>Attitudes and Behaviors:</strong> Always full of energy and loves his family and his work, and is every busy doing something for one or the other. He tirelessly strives to improve his food by getting local ingredients whenever possible, and makes and keeps many repeat customers with his friendly and outgoing demeanor. He is an anchor of the community and always spoken of highly.</p>
+		<p><strong>Frustrations and Needs:</strong> Frank has been highly aware of the presence of new sandwich shops, with the growing local population and rise of larger restaurant chains. He is finding that more young people are losing interest in his restaurant in favor of the relatively new ones. He needs a way to stay competitive, but has no budget for marketing or advertising aside from word of mouth. So, he decided he needs an online presence, professional but inexpensive.</p>
+		<p><strong>Goals:</strong> Frank is looking for a web developer that won't charge a lot, or be willing to accept an alternate arrangement, to give his restaurant a professional and simple online presence with capability to place a pickup order online. He would like to have this done as soon as possible, and will connect with someone nearby with a developer's favorite medium: the Internet!</p>
 	</body>
+
+	<h1>Low Profit Use Case</h1>
+	<p></p>
+	<p></p>
+	<p></p>
 </html>
