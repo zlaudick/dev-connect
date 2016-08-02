4b43f3c418d6be964e838bb4822545fc1116684d
Zac Laudick <zlaudick@nmsu.edu>
2016-08-02 15:33:25 -0600

    added developer use case to epic

--- epic/dev-conn-developer.php
+++ epic/dev-conn-developer.php
@@ -16,5 +16,12 @@
 			<p><strong>Attitudes and Behaviors:</strong> Aside from making himself known among his friend and family; David also attends every networking event that he hears about. He also plans on getting more involved with the community by providing his services at low or no cost; just to gain the experience and build up his portfolio.</p>
 			<p><strong>Frustration and Needs:</strong> David is a little concerned about companies not been able to hire him due to lack of experience. He also takes under consideration the fact that there could be many other developers that are out there with more experience.</p>
 			<p><strong>Goals:</strong> David is eager to demonstrate his talent in the next few weeks. The sooner that he could get hands-on the better for him.</p>
+
+		<h1>Developer Use Case</h1>
+		<p>David T. Ortiz recently graduated from the Deep Dive Coding Bootcamp and has started his job search. After interviewing with potential employers he has found that most employers are looking for developers with more experience than he currently has.</p>
+		<p>David's instructors have told him he can gain experience by building websites for local small businesses and non-profits. They have also sent him a link to the Dev Connect website so he can search for available projects.</p>
+		<p>It's noon on Wednesday and David has plenty of free time due to his current lack of employment. He is using his hp laptop to visit the Dev Connect website and plans to create his profile and start searching the job board for projects he can work on.</p>
+
+
 	</body>
 </html>
