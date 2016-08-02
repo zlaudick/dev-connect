527e29b0fc7f3d1f95f36cb331a8e3abca889ac6
Zac Laudick <zlaudick@nmsu.edu>
2016-08-02 16:04:27 -0600

    added conceptual model php file with html scaffolding

---
+++ epic/dev-conn-conceptual-model.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Dev Connect Conceptual Model</title>
+	</head>
+	<body>
+		<h1>Entities and Attributes</h1>
+		<h1>Conceptual Model</h1>
+	</body>
+</html>
