e28af6fbb86669b967d17ec8b56d170b4682d645
Devon Beets <dbeetzz@gmail.com>
2016-08-02 15:20:18 -0600

    matched dev conn non profit html formatting for consistency to others

--- epic/dev-conn-non-profit.php
+++ epic/dev-conn-non-profit.php
@@ -9,7 +9,8 @@
 	<body>
 		<h1>Organization Persona</h1>
 		<p><strong><em>Persona</em></strong></p>
-		<strong>Age:</strong><p>38</p>
+		<p><strong>Name:</strong></p>
+		<p><strong>Age:</strong></p>
 		<p><strong>Profession:</strong>On the organization side of the project, a typical persona is a board member of a well know local non-profit organization.  Typically this person is female and is middle aged to older.  She works part-time for this non-profit as she has other endeavors as well.  As a result she has a very busy schedule.  However, serving on this board, for a cause she believes in, is very near and dear to her heart and is a labor of love.</p>
 		<p><strong>Technology:</strong>Although she is not technically savvy as a web-developer, she has nonetheless embraced technology and has a strong presence on social media.  As a result, she has been tasked with creating an online presence for her organization.  Since she is already burden with a heavy schedule, she is certainly feeling overwhelmed by this task at hand.</p>
 		<p><strong>Attitudes and Behaviors:</strong>Her first course of action is to look into a do-it-yourself site such as wix.com. DevConnect will be designed with her in mind such that DevConn will show up on her google search.</p>
