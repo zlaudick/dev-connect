3fcd5169938ddac7f8e0c4128c3a60c1918c4c26
Devon Beets <dbeetzz@gmail.com>
2016-08-02 13:42:57 -0600

    added low profit persona php file, changed name of other org file to non profit

---
+++ epic/dev-conn-low-profit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>
+			DevConn For Profit Organization
+		</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<p></p>
+	</body>
+</html>
---
+++ epic/dev-conn-non-profit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>
+			DevConn Non-Profit Organization
+		</title>
+	</head>
+	<body>
+		<h1>Organization Persona</h1>
+		<p>On the organization side of the project, a typical persona is a board member of a well know local non-profit organization.  Typically this person is female and is middle aged to older.  She works part-time for this non-profit as she has other endeavors as well.  As a result she has a very busy schedule.  However, serving on this board, for a cause she believes in, is very near and dear to her heart and is a labor of love.</p>
+		<p>Although she is not technically savvy as a web-developer, she has nonetheless embraced technology and has a strong presence on social media.  As a result, she has been tasked with creating an online presence for her organization.  Since she is already burden with a heavy schedule, she is certainly feeling overwhelmed by this task at hand.</p>
+		<p>Her first course of action is to look into a do-it-yourself site such as wix.com. DevConnect will be designed with her in mind such that DevConn will show up on her google search.</p>
+
+	</body>
+</html>
