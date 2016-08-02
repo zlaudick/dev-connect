50701bf250507b5b7bf5caf0f9560aef4cd6c0c3
Devon Beets <dbeetzz@gmail.com>
2016-08-02 16:28:27 -0600

    updated non profit persona to match styles with other pages, copied over interaction flows

--- epic/dev-conn-non-profit.php
+++ epic/dev-conn-non-profit.php
@@ -8,13 +8,51 @@
 	</head>
 	<body>
 		<h1>Organization Persona</h1>
-		<p><strong>Name:</strong>  Jill Ann Jackson</p>
-		<p><strong>Age:</strong>  48</p>
+		<p><strong>Name:</strong> Jill Ann Jackson</p>
+		<p><strong>Age:</strong> 48</p>
+		<p><strong>Profession:</strong> Jill is the Vice Chairman of the local chapter of the League of World Children Rights Advocacy.  This non-profit organization is involved with promoting children's issues within the inner city. As a board member of this charity, she is involved with important decisions regarding marketing, finances, and fund raising.</p>
+		<p><strong>Technology:</strong> Jill is a mainly a PC laptop user.  She is also an iPhone user and has fully integrated most of its features within her busy life.  Although she is not technically savvy as a web-developer, she has nonetheless embraced technology and has a strong presence on social media. As a result, she has been tasked with creating an online presence for her organization. </p>
+		<p><strong>Attitudes and Behaviors:</strong> Jill has other personal business endeavors as well. As a result, she has a very busy schedule. However, serving on this board, for a cause she believes in, is very near and dear to her heart and is a labor of love.</p>
+		<p><strong>Frustrations and Needs:</strong> Jill is already burden with a heavy schedule.  As a result, she is certainly feeling overwhelmed by this task at hand.  Also, there is a budget shortfall looming this year and she feels personally responsible for that.  Therefore, she does not have the profession website budget.  Therefore, she is looking into low cost alternatives such as a do-it-yourself site such as wix.com.</p>
+		<p><strong>Goals:</strong> Jill is a perfectionist.  She already has in her mind what her organization's website should look like and from her personal experience she feels she has handle on what a good user experience should be for her web site.</p>
 
-		<p><strong>Profession:</strong> Jill is the Vice Chairman of the local chapter of the League of World Children Rights Advocacy.  This non-profit organization is involved with promoting childrens issues within the inner city. As a board member of this charity, she is involved with important decisions regarding marketing, finances, and fund raising.  </p>
-		<p><strong>Technology:</strong>  Jill is a mainly a PC laptop user.  She is also an iPhone user and has fully integrated most of its features within her busy life.  Although she is not technically savvy as a web-developer, she has nonetheless embraced technology and has a strong presence on social media. As a result, she has been tasked with creating an online presence for her organization. </p>
-		<p><strong>Attitudes and Behaviors:</strong>  Jill has other personal business endeavors as well. As a result, she has a very busy schedule. However, serving on this board, for a cause she believes in, is very near and dear to her heart and is a labor of love.</p>
-		<p><strong>Frustrations and Needs:</strong>  Jill is already burden with a heavy schedule.  As a result, she is certainly feeling overwhelmed by this task at hand.  Also, there is a budget shortfall looming this year and she feels personally responsible for that.  Therefore, she does not have the profession website budget.  Therefore, she is looking into low cost alternatives such as a do-it-yourself site such as wix.com.</p>
-		<p><strong>Goals:</strong>  Jill is a perfectionist.  She already has in her mind what her organization's website should look like and from her personal experience she feels she has handle on what a good user experience should be for her web site.</p>
+		<h1>Non Profit Use Case</h1>
+		<p></p>
+		<p></p>
+		<p></p>
+
+		<h1>Interaction Flow</h1>
+		<h3>Sign up Process</h3>
+		<ol>
+			<li>Click Sign Up link.</li>
+			<li>Checks Organization box when prompted for account type.</li>
+			<li>Fill out sign up form including:
+				<ul>
+					<li>email</li>
+					<li>username</li>
+					<li>password</li>
+				</ul>
+			</li>
+		</ol>
+		<h3>Set Up Business Profile Page</h3>
+		<ol>
+			<li>Add profile picture</li>
+			<li>Add contact information</li>
+			<li>Add business location</li>
+			<li>Begin creating projects for developers! :)</li>
+		</ol>
+		<h3>Post Projects to Job Board</h3>
+		<ol>
+			<li>Log in to profile</li>
+			<li>Click on Create New Project button</li>
+			<li>Fill out the project form, including:
+				<ul>
+					<li>Project name</li>
+					<li>Project description</li>
+					<li>Short description of the company</li>
+					<li>If the project is paid or volunteered</li>
+				</ul>
+			</li>
+		</ol>
 	</body>
 </html>
