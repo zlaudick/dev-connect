0a13c727e8ee98672e0a76fe7944d659d0df5922
Gerald Sandoval <gsandoval16@cnm.edu>
2016-08-02 13:16:49 -0600

    Gerald: added organization persona (minor changes)

--- epic/dev-conn-summary.php
+++ epic/dev-conn-summary.php
@@ -3,11 +3,11 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>
-			Dev Connect Summary
+			DevConn Organization
 		</title>
 	</head>
 	<body>
-		<h1>Dev Connect Summary</h1>
+		<h1>DevConn Organization</h1>
 		<p>Dev Connect is a website that connects web developers with small or non-profit organizations that need an online presence. Dev Connect will allow web developers to search projects posted by organizations in need of this content. The businesses can also search for web developers and view their portfolios to find preferred design styles.</p>
 		<p>This site will be designed to promote a symbiotic relationship between organizations that have limited funding for a professional website who need volunteer or low cost work, and entry to mid level web developers looking for work experience and a way to improve and showcase their skills.</p>
 		<h2>Ideas</h2>
