b96d440a94ae0f30a7fed20016ed081bd8ccb277
Devon Beets <dbeetzz@gmail.com>
2016-08-02 15:15:14 -0600

    updated low profit persona to match styles with other persona sites

--- epic/dev-conn-low-profit.php
+++ epic/dev-conn-low-profit.php
@@ -7,7 +7,13 @@
 		</title>
 	</head>
 	<body>
-		<h1>Persona</h1>
-		<p></p>
+		<h1>Low Profit CompanyPersona</h1>
+		<p><strong><em>Persona</em></strong></p>
+		<p><strong>Name:</strong></p>
+		<p><strong>Profession:</strong></p>
+		<p><strong>Technology:</strong></p>
+		<p><strong>Attitudes and Behaviors:</strong></p>
+		<p><strong>Frustrations and Needs:</strong></p>
+		<p><strong>Goals:</strong></p>
 	</body>
 </html>
