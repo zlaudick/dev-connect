d71bb63d1e107e71302758dd92b36d7351aa8621
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-08-02 13:44:10 -0600

    Added the persona for developer on Dev-connect

---
+++ epic/dev-con-developer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>
+			Dev-Conn Developer
+		</title>
+	</head>
+	<body>
+		<h1>Developer Persona</h1>
+		<p><strong><em>Persona</strong></em></p>
+			<p><strong>Name:</strong> David T. Ortiz</p>
+			<strong>Age:</strong> 38
+			<p><strong>Profession:</strong> Junior Developer who just recently graduated Deep Dive Coding Bootcamp. Would like to get known as an enthusiastic developer who can design websites for any organizations that could be willing to use his services and potentially hire him. He'll like to practice his newly acquired skills and demonstrate his talent among his relatives and friends who may need help in his area of  expertise.  Also; David is even willing to do any type of volunteer work for potential non-profit organizations if possible to kick off his career.</p>
+			<p><strong>Technology:</strong> David mainly uses his HP lap top to do this on a regular basis but also has his Compaq computer at home.</p>
+			<p><strong>Attitudes and Behaviors:</strong> Aside from making himself known among his friend and family; David also attends every networking event that he hears about. He also plans on getting more involved with the community by providing his services at low or no cost; just to gain the experience and build up his portfolio.</p>
+			<p><strong>Frustration and Needs:</strong> David is a little concerned about companies not been able to hire him due to lack of experience. He also takes under consideration the fact that there could be many other developers that are out there with more experience.</p>
+			<p><strong>Goals:</strong> David is eager to demonstrate his talent in the next few weeks. The sooner that he could get hands-on the better for him.</p>
+
+
+	</body>
+</html>
