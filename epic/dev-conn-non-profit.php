4b514144a74ed8a92702f20baaa1d249975f910b
Gerald Sandoval <gsandoval16@cnm.edu>
2016-08-02 16:17:07 -0600

    Gerald: revised organization persona

--- epic/dev-conn-non-profit.php
+++ epic/dev-conn-non-profit.php
@@ -7,13 +7,15 @@
 		</title>
 	</head>
 	<body>
-		<h1>Non-Profit Organization Persona</h1>
-		<p><strong>Name:</strong></p>
-		<p><strong>Age:</strong></p>
-		<p><strong>Profession:</strong>On the organization side of the project, a typical persona is a board member of a well know local non-profit organization.  Typically this person is female and is middle aged to older.  She works part-time for this non-profit as she has other endeavors as well.  As a result she has a very busy schedule.  However, serving on this board, for a cause she believes in, is very near and dear to her heart and is a labor of love.</p>
-		<p><strong>Technology:</strong>Although she is not technically savvy as a web-developer, she has nonetheless embraced technology and has a strong presence on social media.  As a result, she has been tasked with creating an online presence for her organization.  Since she is already burden with a heavy schedule, she is certainly feeling overwhelmed by this task at hand.</p>
-		<p><strong>Attitudes and Behaviors:</strong>Her first course of action is to look into a do-it-yourself site such as wix.com. DevConnect will be designed with her in mind such that DevConn will show up on her google search.</p>
-		<p><strong>Frustrations and Needs:</strong></p>
-		<p><strong>Goals:</strong></p>
+		<h1>Organization Persona</h1>
+		<p><strong><em>Persona</em></strong></p>
+		<p><strong>Name:</strong>  Jill Ann Jackson</p>
+		<p><strong>Age:</strong>  48</p>
+
+		<p><strong>Profession:</strong> Jill is the Vice Chairman of the local chapter of the League of World Children Rights Advocacy.  This non-profit organization is involved with promoting childrens issues within the inner city. As a board member of this charity, she is involved with important decisions regarding marketing, finances, and fund raising.  </p>
+		<p><strong>Technology:</strong>  Jill is a mainly a PC laptop user.  She is also an iPhone user and has fully integrated most of its features within her busy life.  Although she is not technically savvy as a web-developer, she has nonetheless embraced technology and has a strong presence on social media. As a result, she has been tasked with creating an online presence for her organization. </p>
+		<p><strong>Attitudes and Behaviors:</strong>  Jill has other personal business endeavors as well. As a result, she has a very busy schedule. However, serving on this board, for a cause she believes in, is very near and dear to her heart and is a labor of love.</p>
+		<p><strong>Frustrations and Needs:</strong>  Jill is already burden with a heavy schedule.  As a result, she is certainly feeling overwhelmed by this task at hand.  Also, there is a budget shortfall looming this year and she feels personally responsible for that.  Therefore, she does not have the profession website budget.  Therefore, she is looking into low cost alternatives such as a do-it-yourself site such as wix.com.</p>
+		<p><strong>Goals:</strong>  Jill is a perfectionist.  She already has in her mind what her organization's website should look like and from her personal experience she feels she has handle on what a good user experience should be for her web site.</p>
 	</body>
 </html>
