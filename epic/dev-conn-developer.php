b6c0b70dc7eef3b6950b566a8566aa28385b18aa
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-08-02 15:54:13 -0600

    Changed name to the persona for developer on Dev-connect

--- epic/dev-conn-developer.php
+++ epic/dev-conn-developer.php
@@ -9,16 +9,16 @@
 	<body>
 		<h1>Developer Persona</h1>
 		<p><strong><em>Persona</strong></em></p>
-			<p><strong>Name:</strong> David T. Ortiz</p>
+			<p><strong>Name:</strong> David E. Hamburg</p>
 			<p><strong>Age:</strong> 38</p>
 			<p><strong>Profession:</strong> Junior Developer who just recently graduated Deep Dive Coding Bootcamp. Would like to get known as an enthusiastic developer who can design websites for any organizations that could be willing to use his services and potentially hire him. He'll like to practice his newly acquired skills and demonstrate his talent among his relatives and friends who may need help in his area of  expertise. Also; David is even willing to do any type of volunteer work for potential non-profit organizations if possible to kick off his career.</p>
-			<p><strong>Technology:</strong> David mainly uses his HP lap top to do this on a regular basis but also has his Compaq computer at home.</p>
+			<p><strong>Technology:</strong> David uses his HP lap top on a regular basis but also has his Compaq computer at home and also uses his i-phone6 mainly for calling and checking his emails; since he's always in the go.</p>
 			<p><strong>Attitudes and Behaviors:</strong> Aside from making himself known among his friend and family; David also attends every networking event that he hears about. He also plans on getting more involved with the community by providing his services at low or no cost; just to gain the experience and build up his portfolio.</p>
 			<p><strong>Frustration and Needs:</strong> David is a little concerned about companies not been able to hire him due to lack of experience. He also takes under consideration the fact that there could be many other developers that are out there with more experience.</p>
 			<p><strong>Goals:</strong> David is eager to demonstrate his talent in the next few weeks. The sooner that he could get hands-on the better for him.</p>
 
 		<h1>Developer Use Case</h1>
-		<p>David T. Ortiz recently graduated from the Deep Dive Coding Bootcamp and has started his job search. After interviewing with potential employers he has found that most employers are looking for developers with more experience than he currently has.</p>
+		<p>David E. Hamburg recently graduated from the Deep Dive Coding Bootcamp and has started his job search. After interviewing with potential employers he has found that most employers are looking for developers with more experience than he currently has.</p>
 		<p>David's instructors have told him he can gain experience by building websites for local small businesses and non-profits. They have also sent him a link to the Dev Connect website so he can search for available projects.</p>
 		<p>It's noon on Wednesday and David has plenty of free time due to his current lack of employment. He is using his hp laptop to visit the Dev Connect website and plans to create his profile and start searching the job board for projects he can work on.</p>
 
