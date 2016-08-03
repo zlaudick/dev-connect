1a7fb0482946ee3852b83c361689990bab158183
Devon Beets <dbeetzz@gmail.com>
2016-08-03 14:59:51 -0600

    wrote out developers instead of the abbreviation

--- epic/dev-conn-summary.php
+++ epic/dev-conn-summary.php
@@ -15,7 +15,7 @@
 		<ul>
 			<li>We will have two primary users, developers and organizations. They will be able to use the site to interact with each other and share content and needs.</li>
 			<li>There will be a jobs board, where organizations can post projects and link them to their profile where interested developers can contact them.</li>
-			<li>Devs will be able to link their work to their profile, so organizations can check it out easily.</li>
+			<li>Developers will be able to link their work to their profile, so organizations can check it out easily.</li>
 			<li>Star rating and review system for organizations to leave feedback on developers.</li>
 			<li>Only allow registered users to see contact information for the job listings.</li>
 			<li>Mailgun and Login with Github</li>
