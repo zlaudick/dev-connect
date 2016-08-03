a455f228941c9b18545763af3e36fec22a1e6bdc
Gerald Sandoval <gsandoval16@cnm.edu>
2016-08-03 15:04:48 -0600

    Gerald: captured entities and attributes

--- epic/dev-conn-conceptual-model.php
+++ epic/dev-conn-conceptual-model.php
@@ -7,30 +7,49 @@
 	<body>
 		<h1>Entities and Attributes</h1>
 		<ol>
-			<li>board</li>
-
 			<li>profile</li>
-			<li>projects</li>
-			<li>
-				review
+			<ul>
+				<li>profileId(primary key)</li>
+				<li>profileName</li>
+				<li>profileHash</li>
+				<li>profileSalt</li>
+				<li>profilePhotoRelPath</li>
+				<li>profileContent</li>
+				<li>profileLocation</li>
+				<li>profileAccountType</li>
+			</ul>
+			<li>project</li>
+			<ul>
+				<li>projectId(primary key)</li>
+				<li>projectProfileId(foriegn key)</li>
+				<li>projectProfilePhotoRelPath(foriegn key)</li>
+				<li>projectName</li>
+				<li>projectContent</li>
+				<li>projectDate</li>
+			</ul>
+			<li>tag</li>
+			<ul>
+				<li>tagId(primary key)</li>
+				<li>tagName</li>
+			</ul>
+			<li>message(weak)</li>
+			<ul>
+				<li>messageSentProfileId(primary composite key)</li>
+				<li>messageRecieveProfileId(primary composite key)</li>
+				<li>messageContent</li>
+			</ul>
+			<li>review(weak)</li>
+			<ul>
+				<li>reviewWriteProfileId(primary composite key)</li>
+				<li>reviewReceiveProfileId(primary composite key</li>
+				<li>reviewContent</li>
+				<li>reviewRating</li>
+			</ul>
+			<li>projectTag(weak)</li>
 				<ul>
-					<li>reviewId</li>
-					<li>reviewUserId</li>
-					<li>reviewRating</li>
-					<li>reviewContent</li>
-					<li>reviewDate</li>
+					<li>projectTagProjectId(primary composite key)</li>
+					<li>projectTagTagId(primary composite key)</li>
 				</ul>
-			</li>
-			<li>
-				user
-				<ul>
-					<li>userId</li>
-					<li>userEmail</li>
-					<li>userHash</li>
-					<li>userName</li>
-					<li>userSalt</li>
-				</ul>
-			</li>
 		</ol>
 		<h1>Conceptual Model</h1>
 	</body>
