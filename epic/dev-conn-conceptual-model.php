42eadb349e789c53288d510074a1edef2aec7f64
Zac Laudick <zlaudick@nmsu.edu>
2016-08-04 17:07:19 -0600

    added profileActivationToken attribute

--- epic/dev-conn-conceptual-model.php
+++ epic/dev-conn-conceptual-model.php
@@ -29,6 +29,7 @@
 			<ul>
 				<li>profileId(primary key)</li>
 				<li>profileAccountType</li>
+				<li>profileActivationToken</li>
 				<li>profileContent</li>
 				<li>profileGithubAccessToken</li>
 				<li>profileGithubUserId</li>
