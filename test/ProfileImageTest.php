d9a25dc023686775747d51bf0b12344c0e9b08d1
Zac Laudick <zlaudick@nmsu.edu>
2016-08-07 13:16:04 -0600

    added variables to ProfileImageTest.php https://app.asana.com/0/161555263951776/164464506248095

--- test/ProfileImageTest.php
+++ test/ProfileImageTest.php
@@ -1,1 +1,34 @@
 <?php
+
+namespace Edu\Cnm\DevConnect\Test;
+
+use Edu\Cnm\DevConnect\Test\{Profile, Image};
+
+// grab the project test parameters
+require_once(DevConnectTest.php);
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/publichtml/php/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the ProfileImage class
+ *
+ * This is a complete PHPUnit test of the ProfileImage class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs
+ *
+ * @see ProfileImage
+ * @author Zac Laudick <zlaudick@cnm.edu>
+ **/
+class ProfileImageTest extends DevConnectTest {
+	/**
+	 * profile that uses the ProfileImage
+	 * @var Profile profile
+	 **/
+	protected $profile = null;
+
+	/**
+	 * image class that stores the ProfileImage
+	 * @var Image image
+	 **/
+	protected $image = null;
+}
