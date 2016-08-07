d8c64231959668d108e750bbec43fcd76a1c475c
Zac Laudick <zlaudick@nmsu.edu>
2016-08-07 17:34:45 -0600

    added constructor to Profile.php https://app.asana.com/0/161555263951776/164464506248089

--- public_html/php/classes/Profile.php
+++ public_html/php/classes/Profile.php
@@ -9,6 +9,7 @@ require_once("autoload.php");
  * @author Zac Laudick <zlaudick@cnm.edu>
  **/
 class Profile {
+	use ValidateDate;
 	/**
 	 * id for this Profile; this is the primary key
 	 * @var int $profileId
@@ -74,4 +75,55 @@ class Profile {
 	 * @var string $profileSalt
 	 **/
 	private $profileSalt;
+
+	/**
+	 * constructor for this Profile
+	 *
+	 * @param int|null $newProfileId id of this Profile or null if new Profile
+	 * @param string $newProfileAccountType account type of this Profile
+	 * @param string $newProfileActivationToken activation token for this Profile
+	 * @param boolean $newProfileApproved approval status of this Profile
+	 * @param int $newProfileApprovedById profileId that approved this Profile
+	 * @param \DateTime|string|null $newProfileApprovedDateTime time stamp when this Profile was approved or null if set to current date and time
+	 * @param string $newProfileContent content of this Profile
+	 * @param string $newProfileEmail email associated with this Profile
+	 * @param string $newProfileGithubAccessToken github access token for this Profile
+	 * @param string $newProfileHash password hash for this Profile
+	 * @param string $newProfileLocation location of this Profile
+	 * @param string $newProfileName name of this Profile
+	 * @param string $newProfileSalt password salt for this Profile
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+	public function __construct(int $newProfileId = null, string $newProfileAccountType, string $newProfileActivationToken, boolean $newProfileApproved, int $newProfileApprovedById, $newProfileApprovedDateTime = null, string $newProfileContent, string $newProfileEmail, string $newProfileGithubAccessToken, string $newProfileHash, string $newProfileLocation, string $newProfileName, string $newProfileSalt) {
+		try {
+			$this->setProfileId($newProfileId);
+			$this->setProfileAccountType($newProfileAccountType);
+			$this->setProfileActivationToken($newProfileActivationToken);
+			$this->setProfileApproved($newProfileApproved);
+			$this->setProfileApprovedById($newProfileApprovedById);
+			$this->setProfileApprovedDateTime($newProfileApprovedDateTime);
+			$this->setProfileContent($newProfileContent);
+			$this->setProfileEmail($newProfileEmail);
+			$this->setProfileGithubAccessToken($newProfileGithubAccessToken);
+			$this->setProfileHash($newProfileHash);
+			$this->setProfileLocation($newProfileLocation);
+			$this->setProfileName($newProfileName);
+			$this->setProfileSalt($newProfileSalt);
+		} catch (\InvalidArgumentException $invalidArgument){
+			// rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch (\RangeException $range) {
+			// rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch (\TypeError $typeError) {
+			// rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch (\Exception $exception) {
+			// rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
 }
