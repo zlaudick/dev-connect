9dcf15d2ec4e7b20a5205f1732ecb6166f1d5c14
Devon Beets <dbeetzz@gmail.com>
2016-08-07 20:30:05 -0600

    wrote the dependent objects of profile and first part of first content test https://app.asana.com/0/161552024814222/164464506248091

--- test/MessageTest.php
+++ test/MessageTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace Edu\Cnm\DevConnect\Test;
 
-use Edu\Cnm\DevConnect\Test\{Message};
+use Edu\Cnm\DevConnect\Test\DevConnectTest;
 
 // grab the project test parameters
 require_once("DevConnectTest.php");
@@ -19,12 +19,58 @@ require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
  * @author Devon Beets <dbeets@cnm.edu>
  **/
 
-class MessageTest extends Message {
+class MessageTest extends DevConnectTest {
 	/**
 	 * content of the Message
 	 * @var string $VALID_MESSAGECONTENT
 	 **/
-	protected $VALID_MESSAGECONTENT = "PHPUnit test great success!";
+	protected $VALID_MESSAGECONTENT = "PHPUnit message content test great success";
+	/**
+	 * content of the updated Message
+	 * @var string $VALID_MESSAGECONTENT2
+	 **/
+	protected $VALID_MESSAGECONTENT2 = "PHPUnit message content test still great success";
+	/**
+	 * timestamp of the Message; starts as null and is assigned later
+	 * @var DateTime $VALID_MESSAGEDATE
+	 **/
+	protected $VALID_MESSAGEDATE = null;
+	/**
+	 * content of the Message subject
+	 * @var string $VALID_MESSAGESUBJECT
+	 **/
+	protected $VALID_MESSAGESUBJECT = "PHPUnit message subject test great success";
+	/**
+	 * Profile that created the Message, this is for foreign key relations
+	 * @var Profile profile
+	 **/
+	protected $profile = null;
+
+	/**
+	 * create dependent objects first before running each test
+	 **/
+	public final function setUp() {
+		//run the default setUp() method first
+		parent::setUp();
+
+		//create and insert a Profile to own the test Message, must figure out what to put for these...
+		$this->profile = new Profile(null, null, null, null, null, null, null, "foo@bar.com", null, null, null, "Los Angeles", "Mark Fischbach", null);
+		$this->profile->insert($this->getPDO());
+
+		//calculate the date using the time the unit test was set up
+		$this->VALID_MESSAGEDATE = new \DateTime();
+	}
+
+	/**
+	 * test inserting valid Message content and verifying that actual MySQL data matches
+	 **/
+	public function testInsertValidMessageContent() {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("message");
+
+		//create a new message and insert into MySQL
+		$message = new $message($this->profile->getProfileId(), $this->VALID_MESSAGECONTENT, $this->VALID_MESSAGEDATE, $this->VALID_MESSAGESUBJECT);
+	}
 
 }
 
