e1af45f68389ea5d9f91af50b55f21b51b9a2382
Devon Beets <dbeetzz@gmail.com>
2016-08-08 14:32:20 -0600

    wrote initial doc block and private variables for the Image class https://app.asana.com/0/161552024814222/164464506248093

--- public_html/php/classes/Image.php
+++ public_html/php/classes/Image.php
@@ -1,1 +1,36 @@
 <?php
+namespace Edu\Cnm\DevConnect;
+
+require_once("autoload.php");
+
+/**
+ * Image class for profile images
+ *
+ * This Image can store the image path and file type of a DevConnect profile's pictures
+ *
+ * @author Devon Beets <dbeetzz@gmail.com>
+ * @version 3.0.0
+ **/
+class Image implements \JsonSerializable {
+	/**
+	 * id for this Image, this is the primary key
+	 * @var int $imageId
+	 **/
+	private $imageId;
+	/**
+	 * path for this Image
+	 * @var string $imagePath
+	 **/
+	private $imagePath;
+	/**
+	 * file type of this Image
+	 * @var string $imageType
+	 **/
+	private $imageType;
+
+	/**
+	 * constructor for this Image
+	 *
+	 * @param int|null
+	 **/
+}
