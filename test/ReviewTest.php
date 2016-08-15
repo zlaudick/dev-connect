7fcfa187061f0316930ed9d44079e199c94440bb
Gerald Sandoval <gsandoval16@cnm.edu>
2016-08-15 13:41:27 -0600

    Changes from Scrum meeting Mon Aug 15 https://app.asana.com/0/161555263951776/164464506248101

--- test/ReviewTest.php
+++ test/ReviewTest.php
@@ -9,7 +9,7 @@ require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
  * Full PHPUnit test for the Review class
  *
  * This is a complete PHPUnit test of the Review class. It is complete because *ALL* mySQL/PDO enabled methods
- * are tested for both invalid and valid inputs.
+ * are tested for both valid and invalid inputs.
  *
  * @see Review
  * @author Gerald Sandoval <gsandoval16@cnm.edu>
@@ -51,11 +51,6 @@ class ReviewTest extends DevConnectTest {
 	 * @var int $VALID_REVIEWRATING
 	 **/
 	protected $VALID_REVIEWRATING = 1;
-	/**
-	 * Profile that created the Review; this is for foreign key relations
-	 * @var Profile profile
-	 **/
-	protected $profile = null;
 	/**
 	 * approved value of the Profile
 	 * @var bool $VALID_PROFILEAPPROVED
@@ -113,7 +108,8 @@ class ReviewTest extends DevConnectTest {
 
 		// query the data from mySQL and enforce the fields match our expectations
 		$pdoReview = Review::getReviewByReceiveProfileIdAndWriteProfileId($this->getPDO(),
-								$review->getReviewReceiveProfileId(), $review->getReviewWriteProfileId());
+																	$review->getReviewReceiveProfileId(),
+																	$review->getReviewWriteProfileId());
 
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("review"));
 		$this->assertEquals($pdoReview->getReviewReceiveProfileId(), $review->getReviewReceiveProfileId());
@@ -121,8 +117,6 @@ class ReviewTest extends DevConnectTest {
 		$this->assertEquals($pdoReview->getReviewContent(), $this->VALID_REVIEWCONTENT);
 		$this->assertEquals($pdoReview->getReviewDateTime(), $this->VALID_REVIEWDATE);
 		$this->assertEquals($pdoReview->getReviewRating(), $this->VALID_REVIEWRATING);
-
-
 	}
 	/**
 	 * test inserting a Review that already exists
@@ -133,13 +127,13 @@ class ReviewTest extends DevConnectTest {
 	public function testInsertInvalidReview() {
 		// create a Review with a non null composite id and watch it fail
 
-			$reviewInvalidRecieve = new Review(DevConnectTest::INVALID_KEY,
+			$reviewInvalidReceive = new Review(DevConnectTest::INVALID_KEY,
 												$this->profileWrite->getProfileId(),
 												$this->VALID_REVIEWCONTENT,
 												$this->VALID_REVIEWDATE,
 												$this->VALID_REVIEWRATING);
 
-			$reviewInvalidRecieve->insert($this->getPDO());
+			$reviewInvalidReceive->insert($this->getPDO());
 
 			$reviewInvalidWrite = new Review($this->profileReceive->getProfileId(),
 												DevConnectTest::INVALID_KEY,
@@ -191,20 +185,20 @@ class ReviewTest extends DevConnectTest {
 	/**
 	 * test updating a Review that does not exists
 	 *
-	 * (apersign here)expectedException PDOException
+	 * @expectedException \TypeError
 	 **/
 	public function testUpdateInvalidReview() {
-		/*
-				$review = new Review(null,
-					null,
-					$this->VALID_REVIEWCONTENT,
-					$this->VALID_REVIEWDATE,
-					$this->VALID_REVIEWRATING);
+/*
+				$review = new Review(DevConnectTest::INVALID_KEY,
+											DevConnectTest::INVALID_KEY,
+*/
+		$review = new Review(null, null,
+									$this->VALID_REVIEWCONTENT,
+									$this->VALID_REVIEWDATE,
+									$this->VALID_REVIEWRATING);
 
-				$review->update($this->getPDO());
-		*/
+		$review->update($this->getPDO());
 	}
-
 	/**
 	 * test creating a Review and then deleting it
 	 **/
@@ -234,18 +228,16 @@ class ReviewTest extends DevConnectTest {
 	/**
 	 * test deleting a Review that does not exist
 	 *
-	 * (apersand here)expectedException PDOException
+	 * @expectedException \TypeError
 	 **/
 	public function testDeleteInvalidReview() {
 		// create a Review and try to delete it without actually inserting it
-		 /*
-		$review = new Review($this->profileReceive->getProfileId(),
-			$this->profileWrite->getProfileId(),
-			$this->VALID_REVIEWCONTENT,
-			$this->VALID_REVIEWDATE,
-			$this->VALID_REVIEWRATING);
+
+		$review = new Review(null, null,
+									$this->VALID_REVIEWCONTENT,
+									$this->VALID_REVIEWDATE,
+									$this->VALID_REVIEWRATING);
 		$review->delete($this->getPDO());
-		*/
 	}
 	/**
 	 * test grabbing a Review by review content
@@ -287,14 +279,12 @@ class ReviewTest extends DevConnectTest {
 		$review = Review::getReviewByReviewContent($this->getPDO(), "you will find nothing");
 		$this->assertCount(0, $review);
 	}
-
 	/**
 	 * test:  query all Reviews
 	 **/
 	public function testGetAllValidReviews() {
 		// count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("review");
-
 
 		// create a new Review and insert to into mySQL
 		$review = new Review($this->profileReceive->getProfileId(),
@@ -309,7 +299,6 @@ class ReviewTest extends DevConnectTest {
 
 $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("review"));
 $this->assertCount(1, $results);
-
 
 		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\DevConnect\\Review", $results);
 
@@ -361,7 +350,6 @@ $this->assertCount(1, $results);
 		// count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("review");
 
-
 		// create a new Review and insert to into mySQL
 		$review = new Review($this->profileReceive->getProfileId(),
 			$this->profileWrite->getProfileId(),
@@ -410,16 +398,19 @@ $this->assertCount(1, $results);
 
 		$receiveId = $review->getReviewReceiveProfileId();
 		$writeId = $review->getReviewWriteProfileId();
+		$reviewDate = 1000 * $this->VALID_REVIEWDATE->getTimestamp();
 
-/*
-		$expectedJson = <<< EOF {"reviewReceiveProfileId": $receiveId, 
-										 "reviewWriteProfileId": $writeId, 
+
+		$expectedJson = <<< EOF
+										{"reviewReceiveProfileId": $receiveId, 
+										 "reviewWriteProfileId": $writeId,  
 										 "reviewContent": "$this->VALID_REVIEWCONTENT",
-										 "ReviewDateTime": "$this->VALID_REVIEWDATE", 
-										 "reviewRating": "$this->VALID_REVIEWRATING"]  EOF;
+										 "reviewDateTime": $reviewDate, 
+										 "reviewRating": "$this->VALID_REVIEWRATING"} 
+EOF;
 
 		$this->assertJsonStringEqualsJsonString($expectedJson, json_encode($pdoReview));
-*/
+
 	}
 
 }
