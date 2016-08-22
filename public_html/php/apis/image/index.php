5cdce2bb85bf07f4445226140b114c9e81c8fb31
Devon Beets <dbeetzz@gmail.com>
2016-08-22 13:27:11 -0600

    wrote the handle for the GET request possibilities for the Image API https://app.asana.com/0/161552024814222/168655807314344

--- public_html/php/apis/image/index.php
+++ public_html/php/apis/image/index.php
@@ -2,7 +2,9 @@
 
 require_once dirname(__DIR__, 2) . "../classes/autoload.php"; /**this don't work still**/
 require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
-require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\DevConnect\Image;
 
 /**
  * API for the Image class
@@ -30,13 +32,37 @@ try {
 	// sanitize input
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 	$imagePath = filter_input(INPUT_GET, "imagePath", FILTER_SANITIZE_STRING);
-	$imageType = filter_input(INPUT_GET, "imageType", FILTER_SANITIZE_STRING);
 
 	// make sure the id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
 		throw (new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
 
+	// handle GET request, if id is present, return that image
+	if($method === "GET") {
+		// set XSRF cookie
+		setXsrfCookie();
+
+		// get a specific image or all images and update reply
+		if(empty($id) === false) {
+			$image = Image::getImageByImageId($pdo, $id);
+			if($image !== null) {
+				$reply->data = $image;
+			}
+		} else if(empty($imagePath) === false) {
+			$images = Image::getImageByImagePath($pdo, $imagePath);
+			if($images !== null) {
+				$reply->data = $images;
+			}
+		}
+	} else if($method === "PUT" || $method === "POST") {
+
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		// make sure image content is available
+	}
 
 }
 
