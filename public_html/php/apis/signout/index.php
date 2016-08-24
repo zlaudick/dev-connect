47ca55a996693cadb0d0fbcc2f5d5f9d7175307c
Devon Beets <dbeetzz@gmail.com>
2016-08-24 15:10:12 -0600

    wrote .htaccess and index.php for signout API https://app.asana.com/0/161555263951776/168655807314355

---
+++ public_html/php/apis/signout/index.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+
+/**
+ * API for the sign up process
+ *
+ * @author Devon Beets <dbeetzz@gmail.com> with code borrowed from Merri J Zibert
+ **/
+
+//prepare the default message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/brewcrew.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	if($method === "GET") {
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+			session_start();
+		}
+		$_SESSION = [];
+		$reply->message = "You are now signed out";
+	}
+	else {
+		throw (new InvalidArgumentException("Invalid HTTP method request"));
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+// encode and return reply to front end caller
+echo json_encode($reply);
