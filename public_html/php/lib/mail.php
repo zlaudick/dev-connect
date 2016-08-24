37b45ed501944fec9022b3d10aa6566ddeb12d5d
Gerald Sandoval <gsandoval16@cnm.edu>
2016-08-24 14:22:58 -0600

    MailGun function updated https://app.asana.com/0/161552024814233/168655807314347

--- public_html/php/lib/mail.php
+++ public_html/php/lib/mail.php
@@ -6,13 +6,18 @@
  * @author Gerald Sandoval <gsandoval16@cnm>
  **/
 
-require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
 
 
-function mail ( $senderName, $senderMail, $receiverName, $receiverMail, $subject, $message) {
+function mail ( $domain, $senderName, $senderMail, $receiverName, $receiverMail, $subject, $message) {
+
+
+	//prepare an empty reply
+	$reply = new stdClass();
+	$reply->status = 200;
 
 	// start the mailgun client
-	$client = new \Http\Adapter\Guzzle6\Client(); //what is this???!!!!!!!!!!
+	$client = new \Http\Adapter\Guzzle6\Client();
 	$mailgun = new \Mailgun\Mailgun($config["mailgunKey"], $client);
 
 	// send the message
@@ -23,29 +28,38 @@ function mail ( $senderName, $senderMail, $receiverName, $receiverMail, $subject
 			"text" => $message
 		]
 	);
-
+/*
 	// inform the user of the result
 	if($result->http_response_code !== 200) {
 		throw(new RuntimeException("unable to send email", $result->http_response_code));
 	}
 	$reply->message = "Thank you for reaching out. I'll be in contact shortly!";
-}  else {
-	throw(new InvalidArgumentException("Invalid HTTP method request", 405));
-}
+	}  else {
+		throw(new InvalidArgumentException("Invalid HTTP method request", 405));
+	}
 
-// update reply with exception information
-} catch(Exception $exception) {
-	$reply->status = $exception->getCode();
-	$reply->message = $exception->getMessage();
-} catch(TypeError $typeError) {
-	$reply->status = $typeError->getCode();
-	$reply->message = $typeError->getMessage();
-}
+	// update reply with exception information
+	} catch(Exception $exception) {
+		$reply->status = $exception->getCode();
+		$reply->message = $exception->getMessage();
+	} catch(TypeError $typeError) {
+		$reply->status = $typeError->getCode();
+		$reply->message = $typeError->getMessage();
+	}
+*/
 
-   return $result->message_id;
+	# Iterate through the results and echo the message IDs.
+	$logItems = $result->http_response_body->items;
+	foreach($logItems as $logItem){
+		$mailMessageId = $logItem->message_id;
+	}
+
+
+   return $mailMessageId;
    //https://github.com/mailgun/mailgun-php
 
 }
+
 
 
 ?>
