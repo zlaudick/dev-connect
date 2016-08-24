7c939a4de75839607d1bd97aefc3b24479d3cff4
Zac Laudick <zlaudick@nmsu.edu>
2016-08-24 13:52:47 -0600

    added boiler plate code to github login https://app.asana.com/0/161555263951776/168655807314341

--- public_html/php/apis/github-login/index.php
+++ public_html/php/apis/github-login/index.php
@@ -48,3 +48,31 @@ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 or FITNESS FOR A PARTICULAR PURPOSE.
 
 See the GNU Lesser General Public License for more details.  **/
+
+require('Client.php');
+require('GrantType/IGrantType.php');
+require('GrantType/AuthorizationCode.php');
+
+const CLIENT_ID     = 'your client id';
+const CLIENT_SECRET = 'your client secret';
+
+const REDIRECT_URI           = 'https://bootcamp-coders.cnm.edu/~zlaudick/dev-connect/public_html/php/apis/github-login/';
+const AUTHORIZATION_ENDPOINT = 'https://github.com/login/oauth/authorize';
+const TOKEN_ENDPOINT         = 'https://github.com/login/oauth/access_token';
+
+$client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET);
+if (!isset($_GET['code']))
+{
+	$auth_url = $client->getAuthenticationUrl(AUTHORIZATION_ENDPOINT, REDIRECT_URI);
+	header('Location: ' . $auth_url);
+	die('Redirect');
+}
+else
+{
+	$params = array('code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI);
+	$response = $client->getAccessToken(TOKEN_ENDPOINT, 'authorization_code', $params);
+	parse_str($response['result'], $info);
+	$client->setAccessToken($info['access_token']);
+//	$response = $client->fetch('https://graph.facebook.com/me');
+//	var_dump($response, $response['result']);
+}
