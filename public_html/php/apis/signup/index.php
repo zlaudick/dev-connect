c9d533c27acf29fcb42156baaddbed90bcbdbd2f
Devon Beets <dbeetzz@gmail.com>
2016-08-24 13:50:20 -0600

    created sanitizer of inputs, and checks to make sure fields were filled out for POST as well as create a new profile for the user https://app.asana.com/0/161555263951776/168655807314350

--- public_html/php/apis/signup/index.php
+++ public_html/php/apis/signup/index.php
@@ -29,4 +29,40 @@ try {
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
+	$profileName = filter_input(INPUT_POST, "profileName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileEmail = filter_input(INPUT_POST, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$confirmPassword = filter_input(INPUT_POST, "confirmPassword", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	//perform the actual post
+	if($method === "POST"){
+
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//check if the user selected developer or non-profit organization for account type and offer developers to sign up with Github?
+
+		//check that the user fields that are required have been sent and filled out correctly
+		if(empty($requestObject->profileName) === true) {
+			throw(new \InvalidArgumentException("Please fill in a name."));
+		} elseif(empty($requestObject->profileEmail) === true) {
+			throw(new \InvalidArgumentException("Please fill in an email."));
+		} elseif(empty($requestObject->password) === true) {
+			throw(new \InvalidArgumentException("Please fill in a password."));
+		} elseif(empty($requestObject->confirmPassword) === true) {
+			throw(new \InvalidArgumentException("Please confirm the password"));
+		} elseif($requestObject->password !== $requestObject->confirmPassword) {
+			throw(new \InvalidArgumentException("Password does not match"));
+		}
+	}
+
+	//create a new profile for the user
+	$profile = new Profile(null, $profileName, $profileEmail);
+	$profile->insert($pdo);
+
+	//create a new user, password salt and hash, and activation token
+	$activation = bin2hex(random_bytes(16));
+	$salt = bin2hex(random_bytes(32));
+
+
 }
