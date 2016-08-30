61484afbb287749ae52c159c62ac58b54ab2fea8
Devon Beets <dbeets@cnm.edu>
2016-08-30 15:06:53 -0600

    updated messages API pathing for require_once of autoload.php

--- public_html/php/apis/message/index.php
+++ public_html/php/apis/message/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__DIR__, 2) . "../classes/autoload.php"; /** DIR not going up the directory correctly **/
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
 require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 
