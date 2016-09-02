059615601179fdcbbd9eb308c0950a3905b4fdc7
Gerald Sandoval <gsandoval16@cnm.edu>
2016-09-02 13:12:13 -0600

    Install the UI Bootstrap Navbar https://app.asana.com/0/161555263951776/168655807314384

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -12,27 +12,34 @@ $DEPTH_DIFFERENCE = $CURRENT_DEPTH - $ROOT_DEPTH;
 $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 ?>
 
-<header>
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container">
+<header ng-controller="navController">
+
+	<!-- bootstrap breakpoint directive to control collapse behavior -->
+	<bootstrap-breakpoint></bootstrap-breakpoint>
+
+	<nav class="navbar navbar-default navbar-inverse">
+		<div class="container-fluid">
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<button type="button" class="navbar-toggle collapsed" aria-expanded="false" ng-click="navCollapsed = !navCollapsed">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="<?php echo $PREFIX;?>">Dev-Connect</a>
-			</div> <!-- navbar-header -->
+				<a class="navbar-brand" href="index">Dev-Connect</a>
+			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="collapse navbar-collapse" uib-collapse="navCollapsed">
+
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="<?php echo $PREFIX;?>">Home</a></li>
-					<li><a href="<?php echo $PREFIX;?>signup/">Sign Up</a></li>
-					<li><a href="<?php echo $PREFIX;?>profile/">Profile</a></li>
-					<li><a href="<?php echo $PREFIX;?>projects/">Projects</a></li>
+					<li><a href="index">Home</a></li>
+					<li><a href="sign-up">Sign Up</a></li>
+					<li><a href="profile">Profile</a></li>
+					<li><a href="projects">Projects</a></li>
+
+					<li><a href="https://github.com/rlewis2892/meow-app" target="_blank">GitHub</a></li>
 				</ul>
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
