63ab7510a05af16f71ee47cae3468f1f5a5c5ac0
Zac Laudick <zlaudick@nmsu.edu>
2016-09-05 14:59:43 -0600

    created service for profile https://app.asana.com/0/161555263951776/168655807314362

--- public_html/angular/services/profile-service.js
+++ public_html/angular/services/profile-service.js
@@ -1,3 +1,22 @@
-/**
- * Created by STEMulus on 9/2/2016.
- */
+app.constant("PROFILE_ENDPOINT", "php/apis/profile/");
+app.service("ProfileService", function($http, PROFILE_ENDPOINT) {
+	function getUrl() {
+		return(PROFILE_ENDPOINT);
+	}
+
+	function getUrlForId(profileId) {
+		return(getUrl() + profileId);
+	}
+
+	this.fetch = function(profileId) {
+		return($http.get(getUrlForId(profileId)));
+	};
+
+	this.update = function(profileId, profile) {
+		return($http.put(getUrlForId(profileId), profile));
+	};
+
+	this.destroy = function(profileId) {
+		return($http.delete(getUrlForId(profileId)));
+	};
+});
