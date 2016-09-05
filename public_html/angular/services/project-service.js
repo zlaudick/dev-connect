d1ee78c231ebcf98e9d1247ce686b73d95aab619
Zac Laudick <zlaudick@nmsu.edu>
2016-09-05 15:10:55 -0600

    created service for project https://app.asana.com/0/161555263951776/168655807314371

--- public_html/angular/services/project-service.js
+++ public_html/angular/services/project-service.js
@@ -1,3 +1,30 @@
-/**
- * Created by STEMulus on 9/2/2016.
- */
+app.constant("PROJECT_ENDPOINT", "php/apis/project");
+app.service("ProjectService", function($http, PROJECT_ENDPOINT) {
+	function getUrl(projectId) {
+		return(PROJECT_ENDPOINT);
+	}
+
+	function getUrlForId(projectId){
+		return(getUrl() + projectId);
+	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.fetch = function() {
+		return($http.get(getUrlForId(projectId)));
+	};
+
+	this.create = function() {
+		return($http.post(getUrl(), project));
+	};
+
+	this.update = function() {
+		return($http.put(getUrlForId(projectId), project));
+	};
+
+	this.destroy = function() {
+		return($http.delete(getUrlForId(projectId)));
+	};
+});
