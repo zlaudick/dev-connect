16cf896bafd20722021275e286182ec3cdcc804d
Devon Beets <dbeets@cnm.edu>
2016-09-06 10:29:08 -0600

    added developers wanted section and changed jumbotron to take up more of the viewport height https://app.asana.com/0/161555263951776/168655807314381

--- public_html/angular/views/home.php
+++ public_html/angular/views/home.php
@@ -17,25 +17,44 @@
 </div>
 
 <!-- Begin Content -->
-	<div class="row">
-		<div class="col-xs-12">
-			<div class="well">
-				<h3 class="text-center">A Professional Online Presence for All Social Causes</h3>
-				<div class="row">
-					<div class="col-xs-12">
-						<div class="well">
-							<p class="text-center">Every social issue has it's due need for exposure, which can be costly
-								for a tight budget. Our goal at DevConnect is to give your non-profit organization an
-								opportunity to share your cause on the most global scale: The Internet.</p>
-							<p class="text-center">For low or no cost at all.*</p>
-							<p class="text-center">By connecting you with our network of dedicated and talented web
-								developers who understand and share compassion with the importance of your work, you can
-								bring your cause to light.</p>
-							<p class="text-center"><a href="sign-up.php">Sign Up Today</a> and make your mission known to
-								the world.</p>
-						</div>
+<div class="row">
+	<div class="col-xs-12">
+		<div class="well">
+			<h3 class="text-center">A Professional Online Presence for All Social Causes</h3>
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="well">
+						<p class="text-center">Every social issue has it's due need for exposure, which can be costly
+							for a tight budget. Our goal at DevConnect is to give your non-profit organization an
+							opportunity to share your cause on the most global scale: The Internet.</p>
+						<p class="text-center">For low or no cost at all.*</p>
+						<p class="text-center">By connecting you with our network of dedicated and talented web
+							developers who understand and share compassion with the importance of your work, you can
+							bring your cause to light.</p>
+						<p class="text-center"><a href="sign-up.php">Sign Up Today</a> and make your mission known to
+							the world.</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
+
+<!-- Developers Wanted & Example Projects-->
+<div class="row">
+	<div class="col-md-4 col-xs-12">
+		<div class="well">
+			<h3 class="text-center">Are YOU a Developer with a cause?</h3>
+			<div class="well">
+				<p class="text-center">We need developers with experience in the field, and dedication in their hearts to
+					support our cause.</p>
+				<p class="text-center">There is no more satisfying feeling than knowing that your work is going toward
+					supporting an important issue that you care about, and that you can make a difference in the world with
+					your art.</p>
+				<p class="text-center">Volunteer work looks great on a resume, and this is the best place you can get it.
+					You don't have to be a senior developer either, mid level or beginners are highly encouraged to join.</p>
+				<p class="text-center">Please <a href="sign-up.php">Sign Up Today</a> and we thank you.</p>
+			</div>
+		</div>
+	</div>
+</div>
