7397e8f3dd616fcdca23240c27e1b5ad4cbfb54d
Zac Laudick <zlaudick@nmsu.edu>
2016-09-08 13:35:23 -0600

    added code to tag-service.js https://app.asana.com/0/161555263951776/168655807314371

--- public_html/angular/services/tag-service.js
+++ public_html/angular/services/tag-service.js
@@ -1,3 +1,30 @@
-/**
- * Created by STEMulus on 9/2/2016.
- */
+app.constant("TAG_ENDPOINT", "php/apis/tag");
+app.service("TagService", function($http, TAG_ENDPOINT) {
+	function getUrl(tagId) {
+		return(TAG_ENDPOINT);
+	}
+
+	function getUrlForId(tagId){
+		return(getUrl() + tagId);
+	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.fetch = function() {
+		return($http.get(getUrlForId(tagId)));
+	};
+
+	this.create = function() {
+		return($http.post(getUrl(), tag));
+	};
+
+	this.update = function() {
+		return($http.put(getUrlForId(tagId), tag));
+	};
+
+	this.destroy = function() {
+		return($http.delete(getUrlForId(tagId)));
+	};
+});
