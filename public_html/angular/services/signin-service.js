ec12e457a25070cbd2c6cf5a7ce4ac0d902e33a9
Zac Laudick <zlaudick@nmsu.edu>
2016-09-08 13:40:32 -0600

    added code to signin-service.js https://app.asana.com/0/161555263951776/168655807314353

--- public_html/angular/services/signin-service.js
+++ public_html/angular/services/signin-service.js
@@ -1,3 +1,11 @@
-/**
- * Created by STEMulus on 9/2/2016.
- */
+app.constant("SIGNIN_ENDPOINT", "php/apis/signin/");
+app.service("SigninService", function($http, SIGNIN_ENDPOINT) {
+	function getUrl() {
+		return(SIGNIN_ENDPOINT);
+	}
+
+	this.signin = function(signin) {
+		console.log("inside signin service");
+		return($http.post(getUrl(), signin));
+	};
+});
