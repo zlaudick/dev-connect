b737ce4cf536597e9a68e063881baf88443e77b0
Zac Laudick <zlaudick@nmsu.edu>
2016-09-08 13:31:11 -0600

    added code to review-service.js https://app.asana.com/0/161555263951776/168655807314373

--- public_html/angular/services/review-service.js
+++ public_html/angular/services/review-service.js
@@ -1,3 +1,30 @@
-/**
- * Created by STEMulus on 9/2/2016.
- */
+app.constant("REVIEW_ENDPOINT", "php/apis/review");
+app.service("ReviewService", function($http, REVIEW_ENDPOINT){
+	function getUrl(reviewWriteProfileId, reviewReceiveProfileId) {
+		return(REVIEW_ENDPOINT);
+	}
+
+	function getUrlForId(reviewWriteProfileId, reviewReceiveProfileId) {
+		return(getUrl() + reviewWriteProfileId + reviewReceiveProfileId);
+	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	}
+
+	this.fetch = function() {
+		return($http.get(getUrlForId(reviewWriteProfileId, reviewReceiveProfileId)));
+	};
+
+	this.create = function() {
+		return($http.post(getUrl(), review));
+	};
+
+	this.update = function() {
+		return($http.put(getUrlForId(reviewWriteProfileId, reviewReceiveProfileId), review));
+	};
+
+	this.destroy = function() {
+		return($http.delete(getUrlForId(reviewWriteProfileId, reviewReceiveProfileId)));
+	};
+});
