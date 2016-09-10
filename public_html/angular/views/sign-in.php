8736d7632a80bb6923d3c62f507b6a6fbc301f11
Gerald Sandoval <gsandoval16@cnm.edu>
2016-09-09 23:35:21 -0600

    Added a sign in form https://app.asana.com/0/161552024814233/168655807314384

--- public_html/angular/views/sign-in.php
+++ public_html/angular/views/sign-in.php
@@ -1,1 +1,81 @@
-<h1>This is the Bloody Sign-IN Page</h1>
+<div class="container">
+	<div class="row">
+		<!--
+		<div class="col-xs-12 col-md-6 col-md-offset-0">
+		-->
+		<div class="col-xs-12 col-md-5 col-md-offset-3">
+			<!--
+			<form id="contact-form" action="../php/mailer.php" method="post" novalidate>
+			-->
+
+
+			<form class="form-horizontal well" id="contact-form" action="../php/mailer.php" method="post" novalidate>
+
+				<h2 align="center">Sign In</h2>
+
+
+
+
+
+				<div class="form-group">
+					<label for="username">User Name <span class="text-danger"></span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="username" name="username" placeholder="Username">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label for="password">Password<span class="text-danger"></span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-key" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="password" name="password" placeholder="Password">
+					</div>
+				</div>
+
+
+				<div class="form-group">
+					<input type="checkbox" checked="checked"> Remember me
+
+
+				</div>
+
+				<div class="form-group" class="row">
+
+					<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Log In</button>
+					<p>Forgot your <a href="#">password?</a></p>
+					<!--
+					<span class="psw" align="right" >Forgot your <a href="#">password?</a></span>
+					<p>Forgot your <a href="#">password?</a></p>
+					<span class="psw">Forgot your <a href="#">password?</a></span>
+					-->
+
+				</div>
+
+
+
+
+
+
+				<h2 align="center">-  or  -<p> </p><a align="center" href="https://github.com/login">
+						<img border="0" alt="GitHub" src="images/GitHub.png" width="313" height="92">
+					</a>
+				</h2>
+			</form>
+
+
+		</div>
+
+
+
+
+	</div> <!-- row -->
+</div>
+
+
+
+
