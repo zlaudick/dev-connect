e866333b9f7c84ff120b42eb51804560943a446b
Devon Beets <dbeetzz@gmail.com>
2016-09-10 15:24:02 -0600

    working on some front end https://app.asana.com/0/161555263951776/168655807314381

--- public_html/angular/controllers/activation-controller.js
+++ public_html/angular/controllers/activation-controller.js
@@ -1,8 +1,3 @@
 app.controller("activationController", ["$routeParams", "$scope", function($routeParams, $scope) {
-	/**
-	 * TEST METHOD! TALCOTT THIS IN THE FINAL PRODUCT!!!!
-	 */
-	$scope.getProfileActivation = function() {
-		return($routeParams.profileActivation);
-	};
+
 }]);
