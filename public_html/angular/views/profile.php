222bb792853e4a4d75b237b48a6196946757c501
Gerald Sandoval <gsandoval16@cnm.edu>
2016-09-10 15:55:56 -0600

    updated Profle Page https://app.asana.com/0/161552024814233/168655807314384

--- public_html/angular/views/profile.php
+++ public_html/angular/views/profile.php
@@ -1,40 +1,33 @@
 
 
 
-
 <!-- Begin Main Content -->
 <div class="row">
 	<div class="col-xs-12">
 		<div class="well well-large">
-			<!-- <div class="col-md-12 col-sm-6"> -->
-				<h3 id="element1"> $profileName</h3>
-
-				<p id="element2" align="right">
-					$jobtitle from $city, $state | $email@$domain.com
-				</p>
-
+			<h3 id="element1" class="text-center well-title">$profileName</h3>
+			<p id="element2">
+				$jobtitle from $city, $state | $email@$domain.com
+			</p>
 			<div class="row">
-				<div class="col-md-4 col-sm-12">
-					<img src="images/profile.PNG" alt="Profile"
-						  class="img-responsive img-square img-thumbnail center-block img-bot-margin">
-				</div><!--col-md-6 col-sm-12-->
+				<div class="col-xs-12">
+					<div class="well">
+						<div class="col-md-4 col-sm-12">
+						<img src="images/profile.PNG" alt="Profile"
+							  class="img-responsive img-square img-thumbnail center-block img-bot-margin">
+							</div>
+						<p>Hello!
+							My name is Devon Beets and I am a web developer from Albuquerque, NM. I have a passion for beautiful
+							design and I love to work on a wide variety of projects!
+							I especially hold a deep place in my heart for people with physical disabilities, my mother has worked
+							with them since I was young and I have aloways looked up to her and her ability to give back to her
+							community with her work and would like to do the same! If you would like to request my service, I can
+							reached at the following contact information below! :)
 
-				<div class="col-md-8 col-sm-12">
-
-					<p>Hello!
-
-						My name is Devon Beets and I am a web developer from Albuquerque, NM. I have a passion for beautiful
-						design and I love to work on a wide variety of projects!
-
-						I especially hold a deep place in my heart for people with physical disabilities, my mother has worked
-						with them since I was young and I have aloways looked up to her and her ability to give back to her
-						community with her work and would like to do the same! If you would like to request my service, I can
-						reached at the following contact information below! :)
-
-						-Devon B</p>
-
-				</div><!--col-md-6 col-sm-12-->
-			</div> <!--row-->
+							-Devon B</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
