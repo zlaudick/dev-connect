2896787e8578eea6a365679659d70c3042cbde86
Devon Beets <dbeetzz@gmail.com>
2016-09-12 14:11:24 -0600

    added php to start the session before loading the head-utils and made it so that if a user is signed in, they don't see the sign up button https://app.asana.com/0/161555263951776/168655807314381

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -22,7 +22,9 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right center">
+					<?php if(empty($_SESSION["profile"]) === true) {?>
 					<li><a href="sign-up">Sign Up</a></li>
+					<?php } ?>
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
 							aria-expanded="false">Profile<span class="caret"></span></a>
