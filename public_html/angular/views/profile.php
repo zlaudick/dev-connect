efca6b497da0a607088b1a7e0d94c91e3548bfff
Devon Beets <dbeetzz@gmail.com>
2016-09-13 11:36:28 -0600

    added revisions to profile view, including a sweet picture :picture:, rearranged the content text, adding sections for contact and links and reviews now https://app.asana.com/0/161555263951776/168655807314381

--- public_html/angular/views/profile.php
+++ public_html/angular/views/profile.php
@@ -1,33 +1,48 @@
-
-
-
-<!-- Begin Main Content -->
+<!-- Begin Title Content -->
 <div class="row">
-	<div class="col-xs-12">
-		<div class="well well-large">
-			<h3 id="element1" class="text-center well-title">Devon Beets </h3>
-			<p id="element2">
-				$jobtitle from $city, $state | $email@$domain.com
-			</p>
-			<div class="row">
-				<div class="col-xs-12">
-						<div class="col-md-4 col-sm-12">
-							<div class="well">
-						<img src="images/profile.PNG" alt="Profile"
-							  class="img-responsive img-square img-thumbnail center-block img-bot-margin">
-						<p>Hello!
-							My name is Devon Beets and I am a web developer from Albuquerque, NM. I have a passion for beautiful
-							design and I love to work on a wide variety of projects!
-							I especially hold a deep place in my heart for people with physical disabilities, my mother has worked
-							with them since I was young and I have aloways looked up to her and her ability to give back to her
-							community with her work and would like to do the same! If you would like to request my service, I can
-							reached at the following contact information below! :)
+	<div class="col-md-4 col-xs-12">
+		<h2 class="text-center">Devon Beets </h2>
+	</div>
+	<div class="col-md-6 col-md-offset-2 col-xs-12">
+		<h3>$jobtitle from $city, $state | $email@$domain.com</h3>
+	</div>
+</div>
 
-							-Devon B
-							</div>
-					</div>
-				</div>
+<!-- Begin Picture and Profile Content -->
+<div class="row">
+	<div class="col-md-4 col-xs-12">
+		<img src="images/DevonCapstoneSelfie.jpg" alt="Profile"
+			  class="img-responsive img-square img-thumbnail center-block img-bot-margin">
+	</div>
+	<div class="col-md-8 col-xs-12">
+		<div class="well well-large">
+			<div class="well">
+				<p>Hello!
+					My name is Devon Beets and I am a web developer from Albuquerque, NM. I have a passion for beautiful
+					design and I love to work on a wide variety of projects!</p>
+				<p>I especially hold a deep place in my heart for people with physical disabilities, my mother has worked
+					with them since I was young and I have always looked up to her and her ability to give back to her
+					community with her work and would like to do the same!</p>
+				<p>If you would like to request my service, I can
+					reached at the following contact information below! :)</p>
+				<p>-Devon B</p>
 			</div>
 		</div>
 	</div>
 </div>
+
+<!-- Begin Contact, Links and Review Sections -->
+<div class="row">
+	<div class="col-md-6 col-xs-12">
+		<div class="well well-large">
+			<div class="well">
+				<h4 class="text-center">Email</h4>
+				<p>devon@devonbeetsdesign.com</p>
+				<h4 class="text-center">Links</h4>
+				<a href="#"></a>
+			</div>
+		</div>
+	</div>
+</div>
+
+
